5a9bba2a407e30eb1a1bb9e17cb2e67f9f1a6c1b
lefmorgan <lef.morgan@gmail.com>
2024-09-30 12:12:13 +0200

    create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'rank',
+    pathMatch: 'full',
+  },
+  {
+    path: 'rank',
+    loadChildren: () =>
+      import('./pages/rank-cat/rank-cat.component').then(
+        (m) => m.RankCatComponent
+      ),
+  },
+  {
+    path: 'cats',
+    loadComponent: () =>
+      import('./pages/list-cats/list-cats.component').then(
+        (m) => m.ListCatsComponent
+      ),
+  },
+  {
+    path: 'match',
+    loadComponent: () =>
+      import('./pages/match-cats/match-cats.component').then(
+        (m) => m.MatchCatsComponent
+      ),
+  },
+  { path: '**', component: NotFoundPageComponent },
+];
