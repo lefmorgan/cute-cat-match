197c3fea31d918da1c32dc0ef93e82d33590ca69
lefmorgan <lef.morgan@gmail.com>
2024-09-30 15:50:25 +0200

    add environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://data.latelier.co/cats.json',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://data.latelier.co/cats.json',
+};
