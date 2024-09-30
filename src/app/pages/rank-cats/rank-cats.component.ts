0e5c9f2e5eb94af61b148725a53398a80124bb7e
lefmorgan <lef.morgan@gmail.com>
2024-09-30 16:11:06 +0200

    rank-cats get all cats

--- src/app/pages/rank-cats/rank-cats.component.ts
+++ src/app/pages/rank-cats/rank-cats.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CatsService } from '../../services/cats.service';
+import { Cats } from '../../models/cat.interfaces';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-rank-cat',
@@ -8,5 +11,8 @@ import { Component } from '@angular/core';
   styleUrl: './rank-cats.component.scss'
 })
 export class RankCatsComponent {
-
+  private readonly catsService = inject(CatsService);
+  cats: Cats = []
+  cat$ = this.catsService.getAllCats()
+  cats$$ = toSignal(this.cat$)
 }
